ea0b9c8e16a872166287fceaf9b81cccc28a6a29
Dallas Gale <dallasgale.digital@gmail.com>
2024-11-15 15:40:53 +1100

    wip

--- src/components/haveAQuestion/index.tsx
+++ src/components/haveAQuestion/index.tsx
@@ -6,10 +6,8 @@ type Question = {
   question: string;
   answer: string;
 };
-interface QuestionProps {
-  questions: Question[];
-}
-const HaveAQuestion = ({ questions }: QuestionProps) => {
+
+const HaveAQuestion = () => {
   return (
     <section className={styles.section} id="have-a-question">
       <div className="content">
@@ -32,3 +30,50 @@ const HaveAQuestion = ({ questions }: QuestionProps) => {
   );
 };
 export default HaveAQuestion;
+
+const questions = [
+  {
+    question: "Who can enter the competition?",
+    answer: "You can apply by filling out the form on the website.",
+  },
+  {
+    question: "What do I need to submit?",
+    answer: "The deadline is 31st of December.",
+  },
+  {
+    question: "Which social media platforms can I use?",
+    answer: "The prize is a 6 month internship at Kayo.",
+  },
+  {
+    question: "What’s the deadline for entries?",
+    answer: "The prize is a 6 month internship at Kayo.",
+  },
+  {
+    question: "When is the shortlisting phase of the talent search?",
+    answer: "The prize is a 6 month internship at Kayo.",
+  },
+  {
+    question: "What is the prize?",
+    answer: "The prize is a 6 month internship at Kayo.",
+  },
+  {
+    question: "How much will I get paid?",
+    answer: "The prize is a 6 month internship at Kayo.",
+  },
+  {
+    question: "What are the judges looking for?",
+    answer: "The prize is a 6 month internship at Kayo.",
+  },
+  {
+    question: "When will the winner be announced?",
+    answer: "The prize is a 6 month internship at Kayo.",
+  },
+  {
+    question: "Can I submit multiple entries?",
+    answer: "The prize is a 6 month internship at Kayo.",
+  },
+  {
+    question: "I don’t have professional equipment—can I still enter?",
+    answer: "The prize is a 6 month internship at Kayo.",
+  },
+];
