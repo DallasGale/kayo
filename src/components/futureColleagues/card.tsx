978dcbeb9d4829b0e30ee1ab3332144860a24a85
Dallas Gale <dallasgale.digital@gmail.com>
2024-11-19 14:29:25 +1100

    wip

--- src/components/futureColleagues/card.tsx
+++ src/components/futureColleagues/card.tsx
@@ -1,4 +1,5 @@
 import styles from "./styles.module.scss";
+import { useSpring, a } from "@react-spring/web";
 
 export interface CardProps {
   name: string;
@@ -10,21 +11,34 @@ const Card = ({ name, role, avatar }: CardProps) => {
   return (
     <div className={styles.cardWrapper}>
       <div className={styles.card}>
-        <div className={styles.cardInner}>
-          <picture className={styles.cardFront}>
-            {avatar !== null && (
-              <img src={avatar} alt={name} className={styles.avatar} />
-            )}
-          </picture>
-        </div>
+        <div>
+          <div className={styles.cardInner}>
+            <div className={styles.cardFront}>
+              <div className={styles.cardAvatar}>
+                <div
+                  className={styles.image}
+                  style={{ backgroundImage: `url(${avatar})` }}
+                />
+              </div>
+            </div>
 
-        {/* <div className={styles.cardBack}>
-          <h1>John Doe</h1>
-          <p>Architect & Engineer</p>
-          <p>We love that guy</p>
-        </div> */}
+            {/* <picture className={styles.cardFront}>
+              {avatar !== null && (
+                <img src={avatar} alt={name} className={styles.avatar} />
+              )}
+            </picture> */}
+
+            <div className={styles.cardBack}>
+              <p>
+                Recently elevated to Legend status in the AFL Hall of Fame,
+                Jason Dunstall’s on-field career speaks for itself. As an
+                expert, Dunstall now adds his analysis on game day and provides
+                plenty of laughs alongside other larrikins on Bounce.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      {/* </div> */}
       <div className={styles.cardNameTitle}>
         <h2 className="display-5">{name}</h2>
         <p>{role}</p>
